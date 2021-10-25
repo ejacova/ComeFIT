cf98b144aecb1c8e8e45aded92ccb7a3d588851c
Alejandro <aeslava849@unab.edu.co>
2021-10-25 18:02:54 -0500

    Se hizo el layout register :3 (Hanki arreglelo :c)♥

---
+++ app/src/main/java/co/edu/ecordoba263/comefit/RegisterActivity.java
@@ -0,0 +1,14 @@
+package co.edu.ecordoba263.comefit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
