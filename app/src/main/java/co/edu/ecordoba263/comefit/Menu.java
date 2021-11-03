6393069483045feb756d810361cc65bf41f20ec2
fcordero <fcordero@unab.edu.co>
2021-11-02 23:20:02 -0500

    Se realizo el diseño del menu principal, el cual se evidencia en activity llamado fragment_menu.xml

--- app/src/main/java/co/edu/ecordoba263/comefit/Menu.java
+++ app/src/main/java/co/edu/ecordoba263/comefit/Menu.java
@@ -1,18 +1,28 @@
 package co.edu.ecordoba263.comefit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Menu extends AppCompatActivity {
+import co.edu.ecordoba263.comefit.fragment.FragmentMenu;
+
+public class Menu extends AppCompatActivity implements FragmentMenu.OnFragmentInteractionListener {
+
+    Fragment fragmentMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        fragmentMenu = new FragmentMenu();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedorFragments,fragmentMenu).commit();
 
     }
 
@@ -20,5 +30,9 @@ public class Menu extends AppCompatActivity {
     public void onBackPressed() {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(Menu.this, LoginActivity.class));
+    }
+
+    public void onFragmentInteraction(Uri uri){
+
     }
 }
